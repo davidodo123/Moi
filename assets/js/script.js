b2b7fab72a0910de02b1e97e6b7f9a3bc0f342b9
davidodo123 <davidsantiagoybermudez@gmail.com>
2026-01-16 18:12:01 +0100

    u

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+  document.addEventListener("DOMContentLoaded", () => {
+    const navIcon = document.getElementById("nav-icon");
+    const mobileMenu = document.querySelector(".menu_mobile");
+
+    if (!navIcon || !mobileMenu) return;
+
+    navIcon.addEventListener("click", () => {
+      navIcon.classList.toggle("open");
+      mobileMenu.classList.toggle("menu-open");
+      document.body.style.overflow = mobileMenu.classList.contains("menu-open")
+        ? "hidden"
+        : "";
+    });
+  });
